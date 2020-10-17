df21b205da1fc8e66a6168bcf909410191fc82f9
root <you@example.com>
2020-10-18 05:13:27 +0530

    added

---
+++ #!groovy.groovy
@@ -0,0 +1,57 @@
+#!groovy
+pipeline {
+    agent any
+    environment {
+        file_listener="dgi.filelistener"
+        file_processor="dgi.fileprocessor"
+        metadata_contentvalidator="dgi.metadatacontentvalidator"
+        json_formatter="dgi.jsonformatter"
+		}
+
+    stages {
+        stage('Preparation') {
+            steps {
+                echo '14'
+            }
+        }
+        stage('Maven Build') {
+            steps {
+                echo 'Maven Build..'
+            }
+        }
+        stage('Sonarqube Scanner') {
+            steps {
+                echo 'Sonarqube Scanner....'
+            }
+        }
+		stage('Fortify Scan') {
+            steps {
+                echo 'Fortify Scan....'
+            }
+        }
+		stage('Copy artifacts') {
+            steps {
+                echo 'Copy artifacts....'
+            }
+        }
+		stage('Deploy in Dev Environemnt') {
+            steps {
+                echo 'Deploying in Dev Environemnt....'
+            }
+        }
+		stage('Deploy in UAT Environemnt') {
+            steps {
+                echo 'Deploying in UAT Environemnt....'
+            }
+        }
+		stage('Deploy in Prod Environemnt') {
+            steps {
+                echo 'Build 12 ....'
+            }
+        }
+		
+		
+	
+	
+	} // stage 12
+} //pipeline
